e20b9a51dbb58557e6729fd57998bde56cea3714
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-01-18 19:25:08 +0800

    现在用 mktemp 生成动态的临时文件，外加带有注释的打印临时文件的路径

--- reference-image.sh
+++ reference-image.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # call a desktop image viewer, to show some image in clipboard
 
-finale_image_path="/tmp/reference-image.bash/screenshort.tmp"
+image_path="/tmp/reference-image.bash/$(mktemp)"
 
-spectacle --background --nonotify --region --output "${finale_image_path}"
-echo "${finale_image_path}"
+spectacle --background --nonotify --region --output "$image_path"
+echo "Temporary image path: ${image_path}"
 
-koko "${finale_image_path}"
+koko "$image_path"
+
+rm --verbose "$image_path"
